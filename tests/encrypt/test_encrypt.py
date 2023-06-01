b8bf1021f91197d3d8da1ff60917571fd71ee7a3
Rita Moura <ritamoura520@gmail.com>
2023-06-01 19:43:09 -0300

    Feat: adiciona teste para a função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message(3, 'message')
+
+    assert encrypt_message('message', 3) == 'sem_egas'
+
+    assert encrypt_message('message', 4) == 'ega_ssem'
+
+    assert encrypt_message('message', 9) == 'egassem'

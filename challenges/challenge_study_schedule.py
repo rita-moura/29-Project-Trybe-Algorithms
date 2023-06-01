cf51ede4c05ad801e3384472d7f2a54df8f6c408
Rita Moura <ritamoura520@gmail.com>
2023-06-01 15:56:38 -0300

    Fix: refatora a função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,14 +5,13 @@ def study_schedule(permanence_period, target_time):
     if not permanence_period:
         return 0
 
-    time_list = []
+    count = 0
     for period in permanence_period:
         if not isinstance(period, tuple) or len(period) != 2 or \
                 any(not isinstance(time, int) for time in period):
             return None
 
         start_time, end_time = period
-        time_list.extend(range(start_time, end_time + 1))
+        count += max(0, min(target_time, end_time) - max(start_time, target_time) + 1)
 
-    count = time_list.count(target_time)
     return count

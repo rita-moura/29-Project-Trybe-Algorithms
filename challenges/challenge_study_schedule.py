ce99d6ce3f86a27a02194d8dbfd7356c3064bd13
Rita Moura <ritamoura520@gmail.com>
2023-06-01 15:04:47 -0300

    Feat: implementa a função study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    if not permanence_period:
+        return 0
+
+    count = 0
+    for period in permanence_period:
+        if not isinstance(period, tuple) or len(period) != 2 or any(not isinstance(time, int) for time in period):
+            return None
+
+        start_time, end_time = period
+        if start_time <= target_time <= end_time:
+            count += 1
+
+    return count

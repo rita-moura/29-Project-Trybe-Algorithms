a45f3663c075d409ee41ffd5e8fce93c142be75c
Rita Moura <ritamoura520@gmail.com>
2023-06-04 12:33:45 -0300

    Fix: refatora study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,25 +1,15 @@
-def is_valid_period(period):
-    return isinstance(period, tuple) and len(period) == 2 and all(
-            isinstance(time, int) for time in period)
+def study_schedule(permanence_period, target_time):
+    if target_time is None:
+        return None
 
-
-def count_valid_periods(permanence_period, target_time):
     count = 0
     for period in permanence_period:
-        if is_valid_period(period):
+        if isinstance(period, tuple) and len(period) == 2 and all(
+                isinstance(time, int) for time in period):
             start_time, end_time = period
             if start_time <= target_time <= end_time:
                 count += 1
         else:
             return None
+
     return count
-
-
-def study_schedule(permanence_period, target_time):
-    if target_time is None:
-        return None
-
-    if not permanence_period:
-        return 0
-
-    return count_valid_periods(permanence_period, target_time)

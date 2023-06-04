e514590210b497e51b86021588fb99abd58cab06
Rita Moura <ritamoura520@gmail.com>
2023-06-04 12:52:01 -0300

    Feat: implementa a função is_palindrome_iterative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,11 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+    start = 0
+    end = len(word) - 1
+    while start < end:
+        if word[start] != word[end]:
+            return False
+        start += 1
+        end -= 1
+    return True

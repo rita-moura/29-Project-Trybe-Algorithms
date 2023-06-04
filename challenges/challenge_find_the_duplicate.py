10826489f4916e908d9c14335c7f1af548f2e4d0
Rita Moura <ritamoura520@gmail.com>
2023-06-04 12:48:11 -0300

    Feat: implementa a função find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) == 1:
+        return False
+
+    duplicate_nums = set()
+    for num in nums:
+        if isinstance(num, str) or num < 1:
+            return False
+        if num in duplicate_nums:
+            return num
+        duplicate_nums.add(num)
+
+    return False
